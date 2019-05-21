82e2372a25b1cca323bf601e760f655eb1c82637
Daniel Hochleitner <dhochleitner@posteo.de>
2019-05-21 11:29:30 +0200

    allow access to tomcat manager ui from outside of the docker container

--- scripts/install_tomcat.sh
+++ scripts/install_tomcat.sh
@@ -13,6 +13,7 @@ sed -i -E 's:#JAVA_HOME#:'${JAVA_HOME}':g' /files/tomcat.service
 sed -i -E 's:#TOMCAT_HOME#:'${TOMCAT_HOME}':g' /files/tomcat.service
 mv /files/tomcat.service /etc/systemd/system/tomcat.service
 
+# create setenv.sh
 echo 'export CATALINA_HOME="'${TOMCAT_HOME}'"' > ${TOMCAT_HOME}/bin/setenv.sh
 echo 'export JAVA_HOME="'${JAVA_HOME}'"' >> ${TOMCAT_HOME}/bin/setenv.sh
 echo 'export CATALINA_OPTS="$CATALINA_OPTS -Xms256m"' >> ${TOMCAT_HOME}/bin/setenv.sh
@@ -22,8 +23,11 @@ echo 'export CATALINA_OPTS="$CATALINA_OPTS -XX:PermSize=128m"' >> ${TOMCAT_HOME}
 echo 'export CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=256m"' >> ${TOMCAT_HOME}/bin/setenv.sh
 chmod a+x ${TOMCAT_HOME}/bin/setenv.sh
 
+# fix some tomcat settings
 echo 'tomcat.util.http.parser.HttpParser.requestTargetAllow=|' >> ${TOMCAT_HOME}/conf/catalina.properties
+sed -i -E 's/127\\.\\d+\\.\\d+\\.\\d+|::1|0:0:0:0:0:0:0:1/.*/g' ${TOMCAT_HOME}/webapps/manager/META-INF/context.xml
 
+# add to path
 echo 'export CATALINA_HOME="'${TOMCAT_HOME}'"' > ${TOMCAT_HOME}/.profile
 echo 'export JAVA_HOME="'${JAVA_HOME}'"' >> ${TOMCAT_HOME}/.profile
 chmod a+x ${TOMCAT_HOME}/.profile
